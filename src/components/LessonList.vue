6b4af630de8539b31f39d12a22074b87c9fa7881
Roni <uranoni777@gmail.com>
2019-07-04 14:56:42 +0800

    add version cart

---
+++ src/components/LessonList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <LessonItem v-for="lesson in lessons" :key="lesson" :lesson="lesson"></LessonItem>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+import LessonItem from "./LessonItem";
+export default {
+  components: {
+    LessonItem
+  },
+  computed: { ...mapState(["lessons"]) },
+  methods: {
+    ...mapActions(["fetchLessons"])
+  },
+  mounted() {
+    this.fetchLessons();
+  }
+};
+</script>
+
+<style scoped>
+.list {
+  padding-top: 30px;
+  text-align: center;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -3,14 +3,25 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const LESSONS_URL = 'https://hiskio.com/api/v1/courses/professions?type=all&level=all&sort=latest&profession_id=1';
+
 export default new Vuex.Store({
   state: {
-
+    lessons: []
   },
   mutations: {
-
+    setLessons(state, lessons) {
+      state.lessons = lessons;
+    }
   },
   actions: {
-
+    fetchLessons({ commit }) {
+      fetch(LESSONS_URL)
+        .then(rs => rs.json())
+        .then(rs => {
+          // const lessons = rs.courses;
+          commit('setLessons', rs.courses)
+        })
+    }
   }
 })
